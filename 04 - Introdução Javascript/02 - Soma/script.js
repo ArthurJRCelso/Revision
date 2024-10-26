a27524510166ae1a22c0abcd8c97de167473953d
arthur <arthur.rodrigues1784@gmail.com>
2024-10-26 02:03:08 +0100

    revisão

---
+++ 04 - Introdução Javascript/02 - Soma/script.js
@@ -0,0 +1,5 @@
+alert("Olá, iremos somas dois números!")
+let numberOne = Number(prompt("Digite o primeiro número:"))
+let numberTwo = Number(prompt("Digite o segundo número:"))
+let result = (numberOne + numberTwo) / 2
+alert("Resultado final: " + result)
