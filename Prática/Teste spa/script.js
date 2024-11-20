5e140d5a3f6df1514abf59658d77ee3a5d197d31
arthur <arthur.rodrigues1784@gmail.com>
2024-11-20 12:30:45 +0000

    substring(1)

--- Prática/Teste spa/script.js
+++ Prática/Teste spa/script.js
@@ -1,63 +1,53 @@
+const buttonsLinks = document.querySelectorAll('a')
 
+buttonsLinks.forEach(button => {
+    button.addEventListener('click', route)
+    
+})
 
-const routes = {
-    '/home': {
-        'content': '<h1>Bem vindo ao home!</h1><p>Esta é a página incial</p>',
+const Routes = {
+    'home': {
+        content: '<h1>Bem vindo a nossa página</h1><p>Estamos aqui para ajudar</p>',
         className: 'home'
     },
 
-    '/about': {
-        'content': '<h1>Sobre nós</h1><p>Informações sobre nós</p>',
-        className: 'about'
-    },
-
-    '/contact': {
-        'content': '<h1>Contato</h1><p>Entre em contato conosco</p>',
-        className: 'contact'
-    },
-
-    '/help': {
-        'content': '<h1>Precisa de ajuda?</h1><button>Acesse o chat</button>',
+    'help': {
+        content: '<h1>Precisa de ajuda? Acesse o link</h1><a>Clique aqui</a>',
         className: 'help'
     },
 
-}
+    'contact': {
+        content: '<h1>Entre em contato conosco!</h1><button>Acesse o chat</button>',
+        className: 'contact'
+    },
 
-const home = document.querySelector('.home')
-const about = document.querySelector('.about')
-const contact = document.querySelector('.contact')
-const help = document.querySelector('.help')
-
-home.addEventListener('click', route)
-about.addEventListener('click', route)
-contact.addEventListener('click', route)
-help.addEventListener('click', route)
-
-
-
-const bodyElement = document.getElementById('body')
-const contentElement = document.querySelector('#app')
-
-function navigateTo(route) {
-   const routesData = routes[route]
-   contentElement.innerHTML = routesData.content
-   bodyElement.className = routesData.className
+    'about': {
+        content: '<h1>Somos uma empresa séria!</h1><p>Explore o site e conheça mais.</p>',
+        className: 'about'
+    }
 }
 
 function route(event) {
-    event = event || window.event
     event.preventDefault()
 
     window.history.pushState({}, "", event.target.href)
+
     handle()
-    
 }
 
 function handle() {
-    let { pathname } = window.location
-    navigateTo(pathname)
+    const { hash } = window.location
+    const localHash = hash.substring(1)
+    toggleLink(localHash)
 }
 
-window.onpopstate = () => handle()
+function toggleLink(data) {
+    const body = document.querySelector('#body')
+    const app = document.querySelector('#app')
+    const dataRoute = Routes[data] || Routes[404]
 
+    app.innerHTML = dataRoute.content
+    body.className = dataRoute.className
+}
 
+handle()
