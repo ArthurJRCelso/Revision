dfa68e560ea776e9c43741d66ae31325404a0454
arthur <arthur.rodrigues1784@gmail.com>
2024-12-15 15:56:50 +0000

    revisando tudo

---
+++ Prática/Estudo/js/script.js
@@ -0,0 +1,18 @@
+import { Route } from "../spa.js";
+
+const route = new Route()
+
+route.add('/', '/pages/home.html')
+route.add('/buy', '/pages/buy.html')
+route.add('/imc', '/pages/imc.html')
+route.add('/media', '/pages/calcMedia.html')
+route.add('/contact', '/pages/contact.html')
+route.add(404, '/pages/404.html')
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', event => {
+        route.route(event)
+    })
+})
+
+route.handle()
---
+++ Prática/Estudo/spa.js
@@ -0,0 +1,27 @@
+export class Route {
+
+    Router = {}
+
+    add(routeName, link) {
+        this.Router[routeName] = link
+    }
+
+    route(event) {
+        event.preventDefault()
+
+        window.history.pushState({}, "", event.target.href)
+
+        this.handle()
+    }
+
+    handle() {
+        const { pathname } = window.location
+        const page = this.Router[pathname] || this.Router[404]
+
+        fetch(page)
+            .then(link => link.text())
+            .then(html => 
+                document.querySelector('#app').innerHTML = html
+            )
+    }
+}
